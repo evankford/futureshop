1f1c65211865afa05e899ee4eae5a3be5028058b
Evan Ford <evankerrickford@gmail.com>
2020-03-26 13:54:42 -0400

    Changes back in from sam store

--- slate.config.js
+++ slate.config.js
@@ -9,11 +9,11 @@ module.exports = {
   'cssVarLoader.liquidPath': ['src/snippets/css-variables.liquid'],
   'webpack.babel.exclude': [/node_modules\/(?!(swiper|dom7|query-string|strict-uri-encode|split-on-first)\/).*/, /assets/],
   'webpack.extend': {
-    resolve: {
-      alias: {
-        jquery: path.resolve('./node_modules/jquery'),
-        'lodash-es': path.resolve('./node_modules/lodash-es'),
-      },
-    },
+    optimization: {
+      splitChunks: {
+        chunks: 'all',
+        minChunks: 2
+      }
+    }
   },
 };
--- src/scripts/layout/theme.js
+++ src/scripts/layout/theme.js
@@ -19,198 +19,251 @@ import '../../styles/theme.scss.liquid';
 import { focusHash, bindInPageLinks } from '@shopify/theme-a11y';
 
 import objectFitImages from 'object-fit-images';
-objectFitImages();
 
-// Common a11y fixes
-focusHash();
-bindInPageLinks();
-
-// Apply a specific class to the html element for browser support of cookies.
-if (navigator.cookieEnabled) {
-  document.documentElement.className = document.documentElement.className.replace(
-    'supports-no-cookies',
-    'supports-cookies',
-  );
-}
-
-/// Remove focus for non-screen-reader hoverers
-function removeFocus() {
-  document.body.classList.add('hover-focus-hidden');
-  window.removeEventListener('mouseover', removeFocus);
-}
-window.addEventListener('mouseover', removeFocus);
-
-
-/// The only JS to work with the mobile menu
-const menuToggler = document.querySelector('.mobile-nav-toggle');
-const menuBg = document.querySelector('.nav-inner__background');
-function toggleMenu() {
-  if (document.body.classList.contains('nav-open')) {
-    document.body.classList.remove('nav-open');
-  } else {
-    document.body.classList.add('nav-open');
-  }
-}
-menuToggler.addEventListener('click', toggleMenu)
-menuBg.addEventListener('click', toggleMenu)
-
-//// Fancy Scrolling effects  
+//// Fancy Scrolling effects
 import AOS from 'aos';
-
-AOS.init({
-  once: true,
-  offset: -200
-});
-
-
-//////Headroom!
-
+//Headroom
 import Headroom from 'headroom.js';
-var headerHt;
-var myHeader = false;
-function initMenu() {
-  myHeader = document.querySelector('[data-section-type=header]');
-  var overlayHeader = false;
-  if (document.body.classList.contains('template-index')  && myHeader.classList.contains('overlay-header')) {   
-    overlayHeader = true;
+
+class Theme {
+  constructor() {
+
+    //Important site sections
+    this.main = document.getElementById('MainContent')
+    this.header = document.querySelector('[data-section-type=header]');
+    this.hero = false;
+    this.footer = false;
+
+    this.utilities()
+    this.init();
   }
-  
-  //Continue if you have a header!
-  
-  if (myHeader) {
-    //Classes
-    myHeader.classList.add('js-fixed');
-    myHeader.classList.add('loaded');
-    
-    var topOffset;
-    
-    
-    function checkScroll() {
-      if (window.scrollY < topOffset) {
-        myHeader.classList.add('overlay-active')
+
+  init() {
+
+    AOS.init({
+      once: true,
+      offset: -200
+    });
+
+    this.mobileMenuInit()
+    // this.buildHeadroom(); //Disabled for this theme
+    this.initRellaxImages();
+    this.initModules();
+
+    this.resizeHandler = this.allResize.bind(this);
+    this.loadHandler = this.allLoad.bind(this);
+    window.addEventListener('resize', this.resizeHandler);
+    window.addEventListener('load', this.loadHandler);
+  }
+
+  utilities() {
+    objectFitImages()
+
+    // Common a11y fixes
+    focusHash();
+    bindInPageLinks();
+
+    // Apply a specific class to the html element for browser support of cookies.
+    if (navigator.cookieEnabled) {
+      document.documentElement.className = document.documentElement.className.replace(
+        'supports-no-cookies',
+        'supports-cookies',
+      );
+    }
+
+    /// Remove focus for non-screen-reader hoverers
+    function removeFocus() {
+      document.body.classList.add('hover-focus-hidden');
+      window.removeEventListener('mouseover', removeFocus);
+    }
+    window.addEventListener('mouseover', removeFocus);
+
+  }
+
+  mobileMenuInit() {
+    /// The only JS to work with the mobile menu
+    const menuToggler = document.querySelector('.mobile-nav-toggle');
+    const menuBg = document.querySelector('.nav-inner__background');
+    function toggleMenu() {
+      if (document.body.classList.contains('nav-open')) {
+        document.body.classList.remove('nav-open');
       } else {
-        myHeader.classList.remove('overlay-active')
+        document.body.classList.add('nav-open');
       }
     }
+    menuToggler.addEventListener('click', toggleMenu)
+    menuBg.addEventListener('click', toggleMenu)
+  }
 
-    
-    //Get topbar
-    var topBar = myHeader.querySelector('.header-above__inner');
-    var offset = topBar.getBoundingClientRect().height;
-    const mainMain = document.getElementById('MainContent');
-    var imageSection = mainMain.querySelectorAll('.image-section')[0]; 
-    if (imageSection) {
-      offset = imageSection.getBoundingClientRect().height;
+  checkForHero() {
+    var allImages = this.main.querySelectorAll('image-section');
+    if (allImages.length > 0) {
+
+      if (this.main.firstElementChild = allImages[0]) {
+        this.hero = allImages[0]
+      }
     }
-    var myHeadroom = new Headroom(myHeader, {
-      "offset": offset,
+  }
+//Headroom stuff
+  buildHeadroom() {
+    this.offset = 0;
+    this.scrollMark = 0;
+    this.overlay = false;
+
+
+    if (this.header && this.header.classList.contains('site-header')) {
+
+      this.headroomCheckOverlay();
+      this.headroomFixHeader();
+      this.headroomInitCheckScroll();
+      this.headroomHandleOffsets();
+      this.headroomInit();
+    }
+
+  }
+
+  headroomCheckOverlay() {
+    this.checkForHero();
+    console.log(this.hero);
+    if (document.body.classList.contains('template-index') && this.header.classList.contains('overlay-header')) {
+      this.overlay = true;
+    }
+    if (this.hero) {
+      this.overlay = true
+    }
+  }
+
+  headroomFixHeader() {
+    if (this.header) {
+      this.header.classList.add('loaded');
+      if (this.overlay == true) {
+        this.header.classList.add('js-fixed');
+      }
+    }
+  }
+
+  headroomCheckScroll() {
+    if (window.scrollY < topOffset) {
+      this.header.classList.add('overlay-active')
+    } else {
+      this.header.classList.remove('overlay-active')
+    }
+  }
+  headroomInitCheckScroll() {
+    var self = this;
+    if (this.overlay == true && this.hero) {
+      this.scrollMark = this.hero.getBoundingClientRect().height;
+      window.addEventListener('scroll', throttle(self.headroomCheckScroll, 100))
+      headroomCheckScroll();
+    }
+  }
+  headroomHandleOffsets() {
+    this.topBar = this.header.querySelector('.header-above__inner');
+    if (this.topBar) {
+      this.offset = 0 + this.topBar.getBoundingClientRect().height;
+    }
+    if (this.hero) {
+      console.log(this.hero)
+      this.offset += this.hero.getBoundingClientRect().height
+    }
+  }
+  headroomInit() {
+
+    var myHeadroom = new Headroom(this.header, {
+      "offset": this.offset,
       "tolerance": 9
     })
     myHeadroom.init();
-
-
-    // menu size handling
-      if (!overlayHeader) {
-        headerHt = myHeader.getBoundingClientRect().height - myHeader.querySelector('.header-above').getBoundingClientRect().height + topBar.getBoundingClientRect().height + "px";
-        document.getElementById('MainContent').style.paddingTop = headerHt;
-      }
-    if (overlayHeader)  {
-      let mainContent = document.getElementById('MainContent');
-      let allSections = mainContent.querySelectorAll('.shopify-section')
-      
-       topOffset = allSections[0].getBoundingClientRect().height;
-      window.addEventListener('scroll', throttle(checkScroll, 100))
-      checkScroll();
-    }
- 
-   
   }
-}
-  
-window.addEventListener('resize', initMenu)
-window.addEventListener('load', function() {
-  setTimeout(() => {
-    initMenu();
-  }, 400);
-})
 
-function initRellaxImages() {
-  var rellaxImages = document.querySelectorAll('.rellax-image');
-  var rellaxBgs = document.querySelectorAll('.rellax-bg');
-  var rellaxFloats = document.querySelectorAll('.rellax-float');
-  if (rellaxImages) {
-    if (rellaxImages.length > 0) {
-      
-      var rellaxImage = new Rellax('.rellax-image', {
-        center: true,
-        speed: -4
-      })
-      window.addEventListener('load', function () {
-        rellaxImage.refresh();
-      });
-    }
+//Resize handling
+  allResize() {
+    initMenu()
   }
-  if (rellaxBgs) {
-    if (rellaxBgs.length > 0) {
-      rellaxBgs.forEach(el => {
-        let offset = el.parentElement.scrollTop;
-        var bg = new Rellax(el, {
+  allLoad() {
+    this.handleURLParams();
+    setTimeout(() => {
+      initMenu()
+    }, 200);
+  }
+
+//Rellax images
+  initRellaxImages() {
+    this.rellaxImages = document.querySelectorAll('.rellax-image');
+    this.rellaxBgs = document.querySelectorAll('.rellax-bg');
+    this.rellaxFloats = document.querySelectorAll('.rellax-float');
+    if (this.rellaxImages) {
+      if (this.rellaxImages.length > 0) {
+
+        var rellaxImage = new Rellax('.rellax-image', {
           center: true,
-          speed: -5
+          speed: -4
         })
         window.addEventListener('load', function () {
-          bg.refresh();
+          rellaxImage.refresh();
         });
-      })
+      }
     }
-  }
-  if (rellaxFloats) {
-    
-    if (rellaxFloats.length > 0) {
-      
-      rellaxFloats.forEach(el=> {
-        let offset = el.parentElement.scrollTop;
-        var float = new Rellax(el, {
-          center: true, 
-          speed: 3
+    if (this.rellaxBgs) {
+      if (this.rellaxBgs.length > 0) {
+        rellaxBgs.forEach(el => {
+          let offset = el.parentElement.scrollTop;
+          var bg = new Rellax(el, {
+            center: true,
+            speed: -5
+          })
+          window.addEventListener('load', function () {
+            bg.refresh();
+          });
         })
-        window.addEventListener('load', function () {
+      }
+    }
+    if (this.rellaxFloats) {
+
+      if (this.rellaxFloats.length > 0) {
+
+        rellaxFloats.forEach(el => {
+          let offset = el.parentElement.scrollTop;
+          var float = new Rellax(el, {
+            center: true,
+            speed: 3
+          })
+          window.addEventListener('load', function () {
             float.refresh();
-        });
+          });
+        })
+      }
+    }
+  }
+
+  handleURLParams() {
+    var parsedURL = queryString.parse(location.search);
+    if (parsedURL.customer_posted == 'true') {
+      var customerSuccessDrawer = new AlertDrawer({
+        type: 'success',
+        class: 'email-drawer',
+        position: 'top',
+        message: window.alertText.customerSuccess
+      })
+    } else if (parsedURL.form_type == 'customer') {
+      console.log('errored');
+      var customerSuccessDrawer = new AlertDrawer({
+        type: 'error',
+        position: 'top',
+        class: 'email-drawer',
+        message: window.alertText.customerFailure
       })
     }
   }
-}
-initRellaxImages();
 
 
-// check for form submits
-
-function handleURLParams() {
-  var parsedURL = queryString.parse(location.search);    
-  if (parsedURL.customer_posted == 'true') {
-    var customerSuccessDrawer = new AlertDrawer({
-      type: 'success',
-      class: 'email-drawer',
-      position: 'top',
-      message: window.alertText.customerSuccess
-    })
-  } else if (parsedURL.form_type == 'customer') {
-    console.log('errored');
-    var customerSuccessDrawer = new AlertDrawer({
-      type: 'error',
-      position: 'top',
-      class: 'email-drawer',
-      message: window.alertText.customerFailure
+  initModules() {
+    Array.from(document.querySelectorAll('[data-module]')).forEach(el => {
+      const name = el.getAttribute('data-module')
+      const Module = require(`../modules/${name}`).default
+      new Module(el)
     })
   }
 }
 
-window.addEventListener('load', handleURLParams);
-///Look for modules in the script, much of the rest of the boilerplate is in modules!
-
-Array.from(document.querySelectorAll('[data-module]')).forEach(el => {
-  const name = el.getAttribute('data-module')
-  const Module = require(`../modules/${name}`).default
-  new Module(el)
-})
+var site = new Theme();
