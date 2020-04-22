ac6c306599629c60ab7cdf1599eb3e506bb34e2b
Evan Ford <evankerrickford@gmail.com>
2020-04-22 11:05:20 -0400

    Ajax cart redirection fix

--- src/scripts/modules/ajax-cart.js
+++ src/scripts/modules/ajax-cart.js
@@ -1,17 +1,14 @@
 import 'unfetch/polyfill';
 import 'es6-promise/auto';
-import queryString from 'query-string'
+import queryString from 'query-string';
 
-
-import ShopCart from '../components/shop-cart'
-import ShopDrawer from '../components/shop-drawer'
-
+import ShopCart from '../components/shop-cart';
+import ShopDrawer from '../components/shop-drawer';
 
 export default class TestModule {
-
   constructor(el) {
     this.el = el;
-    
+
     this.selectors = {
       cartButton: '[data-cart-button]',
       noteToggle: '[data-note-toggle]',
@@ -19,145 +16,171 @@ export default class TestModule {
       cartDrawer: '[data-cart-drawer]',
       productForms: '[data-product-form]',
       formQuantity: '[data-product-quantity]',
-      formId: '[name="id"]'
-    }
-    this.drawer = new ShopDrawer(el, { direction: this.el.getAttribute('data-offscreen')});
+      formId: '[name="id"]',
+    };
+    this.drawer = new ShopDrawer(el, {
+      direction: this.el.getAttribute('data-offscreen'),
+    });
     this.cartState = false;
-    this.noteToggle = this.el.querySelector('[data-note-toggle]')
+    this.noteToggle = this.el.querySelector('[data-note-toggle]');
     this.init();
-    
-    
   }
-  
+
   init() {
-    this.initCart().then(done=> {
+    this.initCart().then((done) => {
+      this.checkInitialState();
       this.buttonListeners();
       this.formListeners();
     });
   }
-  
+
   async initCart() {
     var self = this;
     document.body.classList.add('ajax-cart');
     self.cart = new ShopCart(self.el.querySelector('[data-cart-drawer]'));
-    var initDone = await self.cart.init().then(resp=> {
-      self.checkCartState(); 
-    }).catch(err=> { self.checkCartState()});
-    
+    var initDone = await self.cart
+      .init()
+      .then((resp) => {
+        self.checkCartState();
+      })
+      .catch((err) => {
+        self.checkCartState();
+      });
+
     return await initDone;
-    
   }
-  
+
   checkCartState() {
     if (this.cart.state) {
-      this.cartState = 'loaded'
+      this.cartState = 'loaded';
     } else {
-      this.cartState = false
-    } 
+      this.cartState = false;
+    }
   }
- 
+
   setupCartButtons() {
     const allAnchors = Array.from(document.getElementsByTagName('a'));
-    allAnchors.forEach(myAnchor=> {
+    allAnchors.forEach((myAnchor) => {
       var hrefString = myAnchor.href;
-      if (hrefString.indexOf("/cart") >=0) {
-        myAnchor.setAttribute(this.cleanSelector(this.selectors.cartButton), true);
+      if (hrefString.indexOf('/cart') >= 0) {
+        myAnchor.setAttribute(
+          this.cleanSelector(this.selectors.cartButton),
+          true
+        );
         if (hrefString.indexOf('change?') !== -1) {
-          const props = queryString.parse(hrefString.slice(hrefString.indexOf('change?') +7 ))
-          myAnchor.setAttribute('data-line-item', props.line)
-          myAnchor.setAttribute('data-line-item-quantity', props.quantity)
+          const props = queryString.parse(
+            hrefString.slice(hrefString.indexOf('change?') + 7)
+          );
+          myAnchor.setAttribute('data-line-item', props.line);
+          myAnchor.setAttribute('data-line-item-quantity', props.quantity);
         }
       }
-    })
-  
+    });
   }
-  
+
+  checkInitialState() {
+    var parsed = queryString.parse(window.location.search);
+    if (parsed.cartOpen) {
+      this.drawer.openDrawer();
+    }
+  }
+
   cleanSelector(selector) {
     return selector.replace('[', '').replace(']', '');
   }
-  
+
   handleCartButtonClick(e) {
-    e.preventDefault()
+    e.preventDefault();
     const realButton = this.getRealButton(e.target);
     const buttonType = this.getButtonType(realButton);
     if (buttonType == 'basic') {
-      this.drawer.openDrawer(); 
+      this.drawer.openDrawer();
     } else {
     }
   }
-  
+
   getRealButton(target) {
-    if (target.getAttribute(this.cleanSelector(this.selectors.cartButton)) == true) return target;
-    return target.closest(this.selectors.cartButton)
+    if (
+      target.getAttribute(this.cleanSelector(this.selectors.cartButton)) == true
+    )
+      return target;
+    return target.closest(this.selectors.cartButton);
   }
   getButtonType(button) {
-    if (typeof button.getAttribute('data-line-item') !== "string"  && typeof button.getAttribute('data-line-item') !== "number") {
-      return 'basic'
+    if (
+      typeof button.getAttribute('data-line-item') !== 'string' &&
+      typeof button.getAttribute('data-line-item') !== 'number'
+    ) {
+      return 'basic';
     } else {
       if (button.getAttribute('data-line-item-quantity') == 0) {
-        return 'remove'
-      } 
+        return 'remove';
+      }
     }
-   }
-  
+  }
+
   setupNote() {
     var noteToggle = this.el.querySelector(this.selectors.noteToggle);
     var noteWrap = this.el.querySelector(this.selectors.noteWrap);
     if (noteToggle && noteWrap) {
-      noteToggle.addEventListener('click', function () {
+      noteToggle.addEventListener('click', function() {
         if (noteWrap.classList.contains('active')) {
-          noteWrap.classList.remove('active')
+          noteWrap.classList.remove('active');
         } else {
           noteWrap.classList.add('active');
         }
-      })
+      });
     }
   }
-  
+
   buttonListeners() {
     //find cart buttons
     this.setupCartButtons();
     this.setupNote();
     var allCartButtons = document.querySelectorAll('[data-cart-button]');
-    allCartButtons.forEach(cartButton=> {
-      cartButton.addEventListener("click", this.handleCartButtonClick.bind(this))
-    })    
-   
+    allCartButtons.forEach((cartButton) => {
+      cartButton.addEventListener(
+        'click',
+        this.handleCartButtonClick.bind(this)
+      );
+    });
   }
-  
-  async setupFormSubmit(formEl) {
-    var toSubmit = {}
-    toSubmit = {
-      productId: parseInt(formEl.querySelector(this.selectors.formId).getAttribute('data-productid')),
-      variantId: parseInt(formEl.querySelector(this.selectors.formId).value),
-      quantity: parseInt(formEl.querySelector(this.selectors.formQuantity).value)
-    }
-    return toSubmit
-  }
-  
 
-  
+  async setupFormSubmit(formEl) {
+    var toSubmit = {};
+    toSubmit = {
+      productId: parseInt(
+        formEl
+          .querySelector(this.selectors.formId)
+          .getAttribute('data-productid')
+      ),
+      variantId: parseInt(formEl.querySelector(this.selectors.formId).value),
+      quantity: parseInt(
+        formEl.querySelector(this.selectors.formQuantity).value
+      ),
+    };
+    return toSubmit;
+  }
+
   formListeners() {
     var self = this;
     self.productForms = document.querySelectorAll(self.selectors.productForms);
-      //Only override form submit if cartstate is not false
-    self.productForms.forEach(form=> {
+    //Only override form submit if cartstate is not false
+    self.productForms.forEach((form) => {
       form.addEventListener('submit', function(e) {
         self.checkCartState();
         if (self.cartState != false) {
           e.preventDefault();
-          self.setupFormSubmit(form).then(resp=> {
-            self.changeCart(resp, {type: 'submitItem'})
-          })
+          self.setupFormSubmit(form).then((resp) => {
+            self.changeCart(resp, { type: 'submitItem' });
+          });
         }
-      })
-    })
+      });
+    });
   }
-  
+
   changeCart(obj, args) {
     this.drawer.openDrawer();
-    this.cart.submitFormObject(obj, args).then(resp=> {
-      
-    });
+    this.cart.submitFormObject(obj, args).then((resp) => {});
   }
 }
