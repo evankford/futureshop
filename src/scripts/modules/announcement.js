6da6d619edfd044b8ed90be245dcb8d70126f0c1
Evan Ford <evankerrickford@gmail.com>
2020-04-24 12:26:53 -0400

    Tweaking announcement bar

--- src/scripts/modules/announcement.js
+++ src/scripts/modules/announcement.js
@@ -1,43 +1,66 @@
+import queryString from 'query-string';
 
-export default class TestModule {
+export default class Announcement {
   constructor(el) {
-    this.el = el
-    this.slideIn = this.el.getAttribute("data-slide-in")
-    this.closer = this.el.querySelector('[data-close-announcement]')
+    this.el = el;
+
+    this.fixedHeader = document.querySelector('.header-fixed');
+    this.slideIn = this.el.getAttribute('data-slide-in');
+    this.closer = this.el.querySelector('[data-close-announcement]');
+    this.active = this.checkIfActive();
     this.init();
+
+    this.enable = this.enableBar.bind(this);
+    this.disable = this.disableBar.bind(this);
+    this.resize = this.resizeBar.bind(this);
+  }
+  checkIfActive() {
+    var parsedString = queryString.parse(window.location.search);
+
+    if (parsedString.showAnnouncement === 'true') {
+      return true;
+    }
+    if (sessionStorage.getItem('announcementDismissed') == 'true') {
+      return false;
+    }
+    return true;
   }
   init() {
+    console.log(this.active);
+    if (!this.active) {
+      return false;
+    }
+
     this.ht = this.el.getBoundingClientRect().height;
-    document.body.style.WebkitTransition = 'margin 300ms ease';
-    document.body.style.MozTransition = 'margin 300ms ease';
-    document.body.style.transition = 'margin 300ms ease';
-  
-    window.addEventListener('load', () => {
-      setTimeout(() => {
-        this.enable();
-      }, 500)
-    })
+    setTimeout(() => {
+      this.enable();
+    }, 500);
     window.addEventListener('resize', () => {
-        this.resize();
-    })
+      this.resize();
+    });
   }
-  enable() {
-    this.el.classList.add('announceActive');
-    this.resize();
-    if (this.closer) {
-      this.closer.addEventListener('click', this.disable.bind(this));
+
+  enableBar() {
+    if (this.active) {
+      this.el.classList.add('announceActive');
+      this.resize();
+      document.body.style.overflowY = 'visible';
+      if (this.closer) {
+        this.closer.addEventListener('click', this.disable.bind(this));
+      }
     }
   }
-  disable() {
+  disableBar() {
     document.body.style.marginTop = '0px';
-    this.el.classList.remove('announceActive')
+    document.body.style.overflowY = 'auto';
+    this.el.classList.remove('announceActive');
+    sessionStorage.setItem('announcementDismissed', 'true');
   }
-  resize() {
+  resizeBar() {
     this.ht = this.el.getBoundingClientRect().height;
-    if (this.el.classList.contains("announceActive")) {
-      this.el.style.top = -this.ht + 'px'
+    if (this.el.classList.contains('announceActive')) {
+      this.el.style.top = -this.ht + 'px';
       document.body.style.marginTop = this.ht + 'px';
-      
     }
   }
 }
