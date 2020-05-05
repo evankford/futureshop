c293d94eb800c5cd4758ffd221d400524fabe342
Evan Ford <evankerrickford@gmail.com>
2020-05-05 10:47:14 -0400

    Ava Max

--- src/scripts/modules/product-image-wrap.js
+++ src/scripts/modules/product-image-wrap.js
@@ -17,7 +17,7 @@ export default class myModule {
     this.init();
   }
   init() {
-   
+
    if (this.imageSwiper) {
      if (this.images.length > 1) {
        this.initSwiper();
@@ -25,13 +25,13 @@ export default class myModule {
    }
 
   }
-  
+
   initSwiper() {
     //REmove shopify hide class.
     this.images.forEach(image=> {
       image.classList.remove('hide');
     });
-    
+
     //Thumb stuff first
     let thumbSwipe= false; //Default false
     if (this.thumbSwiper) {
@@ -46,24 +46,24 @@ export default class myModule {
           prevEl: littleLeft
         }
       }
-      
+
       //Init thumb swiper
       thumbSwipe = new Swiper(this.thumbSwiper, {
-        centeredSlides: true,
        slidesPerView: 'auto',
        loop: false,
        spaceBetween: 1,
        threshold: 10,
        effect: 'slide',
+       centerInsufficientSlides: true,
        slideToClickedSlide: true,
         navigation: littleNav
      })
-     
-     
-      
-      
+
+
+
+
     } // ENd thumbnail checker
-    
+
     //Pagination/Navigation
     let bigNav = false;
     let bigPage = false;
@@ -72,7 +72,7 @@ export default class myModule {
     const bigLeft = this.imageSwiper.querySelector('.swiper-button-prev');
     const bigRight = this.imageSwiper.querySelector('.swiper-button-next');
 
-    
+
     if (bigLeft && bigRight) {
       bigNav = {
         nextEl: bigRight,
@@ -86,20 +86,20 @@ export default class myModule {
       }
     }
     if (thumbSwipe) {
-      bigThumbs = { 
+      bigThumbs = {
         swiper: thumbSwipe
       }
     }
     this.mainSwiper = new Swiper(this.imageSwiper, {
       slidesPerView:1,
-      loop: false, 
+      loop: false,
       centeredSlides: true,
       navigation: bigNav,
       centerInsufficientSlides: true,
       pagination: bigPage,
       thumbs: bigThumbs
     })
-    
+
     var classObserver = new MutationObserver(mutations => {
       mutations.forEach(mutation => {
         if (mutation.type == 'attributes' && mutation.attributeName == 'aria-current') {
